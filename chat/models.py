5d57d3a361cb59220038ee7036429e3a3b66e9ac
jojo157 <carrigj@tcd.ie>
2021-09-20 19:03:02 +0000

    added models for chatroom

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=1000)
+
+
+class Message(models.Model):
+    message_content = models.CharField(max_length=1000000)
+    date_of_message = models.DateTimeField(default=datetime.now, blank=True)
+    user_name = models.CharField(max_length=100)
+    room_name = models.CharField(max_length=100)
+
+
