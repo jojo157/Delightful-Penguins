27d16ee8e1e92a98f27c05502a8be391db90a708
jojo157 <carrigj@tcd.ie>
2021-10-06 22:41:22 +0000

    added reply view for artist to reply to current chat messages and relevant tempaltes

--- artist/views.py
+++ artist/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404, get_list_or_40
 from chat.models import Message, Artist
 from django.http import JsonResponse
 from django.core import serializers
+from django.contrib import messages
+import json 
 
 from chat.views import chatSend
 # Create your views here.
@@ -17,5 +19,30 @@ def artist_status(request):
         status = request.POST['status']
         print(status)
         Artist.objects.filter(pk=1).update(status=status)
-        response = "success"
         return HttpResponse(status=200)
+
+
+def reply(request, id):
+    message = Message.objects.get(pk=id)
+    context = {
+        'new_chat_message': message.message_content,
+        'new_user': message.user_name,
+        'new_chat_session': message.chat_session,
+        'id': id
+    }
+    return render(request, 'reply.html', context) 
+
+def artist_send_reply(request):
+    if request.method == 'POST':
+        id = request.POST.get['message-id']
+        old_message = Message.objects.get(pk=id)
+        message_content = request.POST.get['session-id']
+        chat_session = old_message.chat_session
+        user_name = "Leticia"
+        new_message = Message(message_content=message_content, chat_session=chat_session, user_name=user_name, reply_recieved=True)
+        new_message.save()
+        Message.objects.filter(pk=id).update(reply_recieved=True)
+        messages.success(request, "reply sent")
+
+    return render(request, 'artistMessages.html') 
+        
--- static/js/artist.js
+++ static/js/artist.js
@@ -1,7 +1,6 @@
 $('.status-button').on('click', function(event){
     event.preventDefault();
     var status = $(this).attr('id');
-    var action = $(this).attr('href');
     $.ajax({
         url: 'artist_status/',
         type: 'POST',
@@ -18,7 +17,41 @@ $('.status-button').on('click', function(event){
         }
     }) 
 });
- 
- 
 
+/*
+$('.message-reply-button').on('click', function(event){
+    event.preventDefault();
+    var chat_session = $(this).attr('data-chat-session');
+    var id = $(this).attr('data-chat-id');
+    
+    $.ajax({
+        url: 'reply/',
+        type: 'POST',
+        data: {
+            chat_session: chat_session,
+            id: id,
+            csrfmiddlewaretoken: document.querySelector('input[name="csrfmiddlewaretoken"]').value
+        },
+        success: function(response){
+            data = JSON.parse(response);
+            console.log("success");
 
+           
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    }) 
+});
+*/
+
+$('.message-reply-button').on('click', function(event){
+    event.preventDefault();
+    var chat_session = $(this).attr('data-chat-session');
+    var id = $(this).attr('data-chat-id');
+
+    var href = '/artist/reply';
+
+    location.href = href + "/" + id ;
+
+});
