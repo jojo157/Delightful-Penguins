efebd4a1b1e4fc2c253d7bfe611410e45ae1a5fd
jojo157 <carrigj@tcd.ie>
2021-10-07 12:56:59 +0000

    added login required decorator for views in artist app with if statmeenh if statement to check superuser to prevent unauthorised access

--- artist/views.py
+++ artist/views.py
@@ -3,46 +3,57 @@ from chat.models import Message, Artist
 from django.http import JsonResponse
 from django.core import serializers
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 import json 
 
 from chat.views import chatSend
-# Create your views here.
 
+
+@login_required
 def artist_home(request):
-    #This function will return the Artist Message page
-    #Need to implement only if super user for security
-    return render(request, 'artistMessages.html') 
+#This function returns the Artist Message page if user is super user (artist)
+    if request.user.is_superuser:
+        return render(request, 'artistMessages.html') 
 
-
+@login_required
 def artist_status(request):
-    if request.method == 'POST' and request.is_ajax:
-        status = request.POST['status']
-        print(status)
-        Artist.objects.filter(pk=1).update(status=status)
-        return HttpResponse(status=200)
+#This function updates the user status to Offline or Online depending on the status recieved
+    if request.user.is_superuser:
+        if request.method == 'POST' and request.is_ajax:
+            status = request.POST['status']
+            print(status)
+            Artist.objects.filter(pk=1).update(status=status)
+            return HttpResponse(status=200)
 
-
+@login_required
 def reply(request, id):
-    message = Message.objects.get(pk=id)
-    context = {
-        'new_chat_message': message.message_content,
-        'new_user': message.user_name,
-        'new_chat_session': message.chat_session,
-        'new_id': id
-    }
-    return render(request, 'reply.html', context) 
+#This function renders a page with the message details that the artist has choosen to reply to.
+    if request.user.is_superuser:
+        message = Message.objects.get(pk=id)
+        context = {
+            'new_chat_message': message.message_content,
+            'new_user': message.user_name,
+            'new_chat_session': message.chat_session,
+            'new_id': id
+        }
+        return render(request, 'reply.html', context) 
 
+
+@login_required
 def artist_send_reply(request):
-    if request.method == 'POST':
-        id = request.POST['message-id']
-        old_message = Message.objects.get(pk=id)
-        message_content = request.POST['message']
-        chat_session = old_message.chat_session
-        user_name = "Leticia"
-        new_message = Message(message_content=message_content, chat_session=chat_session, user_name=user_name, reply_recieved=True)
-        new_message.save()
-        Message.objects.filter(pk=id).update(reply_recieved=True)
-        
-        messages.success(request, "reply sent")
-    return redirect('artist_home')
+#This function adds a reply message to the message the artist replied to.
+#The function updates the reply recieved field to True for the message that a reply was sent to
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            id = request.POST['message-id']
+            old_message = Message.objects.get(pk=id)
+            message_content = request.POST['message']
+            chat_session = old_message.chat_session
+            user_name = "Leticia"
+            new_message = Message(message_content=message_content, chat_session=chat_session, user_name=user_name, reply_recieved=True)
+            new_message.save()
+            Message.objects.filter(pk=id).update(reply_recieved=True)
+            
+            messages.success(request, "reply sent")
+        return redirect('artist_home')
         
