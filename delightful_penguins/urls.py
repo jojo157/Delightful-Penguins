a362821c861fe0a90c64d2821e799b0deced82fa
jojo157 <carrigj@tcd.ie>
2021-10-08 21:32:48 +0000

    updated static settings

--- delightful_penguins/urls.py
+++ delightful_penguins/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,5 +25,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('artist/', include('artist.urls')),
     path('art/', include('art.urls')),
-
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
