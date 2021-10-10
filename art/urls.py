4298a3037bfd35a0f58f4710190ee271836c392d
jojo157 <carrigj@tcd.ie>
2021-10-10 16:51:10 +0000

    added functionality to edit art pieces

--- art/urls.py
+++ art/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.art, name='art'),
     path('addArt/', views.addArt, name='addArt'),
+    path('editArt/<int:id>/', views.editArt, name='editArt'),
     path('chatSend/', views.chatSend, name='chatSend'), 
 
 
--- art/views.py
+++ art/views.py
@@ -14,7 +14,7 @@ from .models import Art
 
 def art(request):
     artCollection = Art.objects.all()
-    context={
+    context ={
         'artCollection': artCollection
     }
     return render(request, 'art.html', context)
@@ -29,7 +29,29 @@ def addArt(request):
             return redirect('art')
     else:
         form = ArtForm()
-        context= {
+        context = {
             'form': form,
         }
         return render(request, 'artAdd.html', context)  
+
+
+def editArt(request, id):
+    art = get_object_or_404(Art, pk=id)
+
+    if request.method == 'POST':
+        form = ArtForm(request.POST, request.FILES, instance=art)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Art details updated.")
+            return redirect('art')
+        else:
+            messages.error(request, "Issue updating art piece, please try again later.")
+            return redirect('art')
+
+    else:
+        form = ArtForm(instance=art)
+        context = {
+            'form': form,
+            'id': id,
+        }
+        return render(request, 'artEdit.html', context)
