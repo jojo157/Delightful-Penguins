0241b37a2cf7bd9b0df04edb2a3ae3ffe2303de0
jojo157 <carrigj@tcd.ie>
2021-10-11 14:07:23 +0000

    updated cart template and styling for table

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('chatsend/', views.chatSend, name='chatSend'),
     path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),    
     
 ]
