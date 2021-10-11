7b9a4039e6465f8818346d3e486234c63bf54c95
jojo157 <carrigj@tcd.ie>
2021-10-11 14:56:14 +0000

    added sort on messages in context processor to give messages in date order

--- chat/custom_context_processor.py
+++ chat/custom_context_processor.py
@@ -6,7 +6,7 @@ def get_messages(request):
     artist_status = Artist.objects.get(pk=1)
     if 'ip_address' in request.session:
         chat_session = request.session['ip_address']
-        data = get_list_or_404(Message, chat_session=chat_session)
+        data = get_list_or_404(Message.objects.order_by('date_of_message'), chat_session=chat_session)
         context = {
             'chat_messages': data,
             'artist_status': artist_status,
