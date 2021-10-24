87de0f12dd655299d418bab1efca750df5fdfb81
jojo157 <carrigj@tcd.ie>
2021-10-24 10:03:24 +0000

    added object position to hero image to change what part of image in view

--- delightful_penguins/settings.py
+++ delightful_penguins/settings.py
@@ -130,10 +130,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if "USE_AWS" in os.environ:
     # cache control
-   # AWS_S3_OBJECT_PARAMETERS = {
-    #    "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
-   #     "CacheControl": "max-age=94608000",
-   # }
+    #AWS_S3_OBJECT_PARAMETERS = {
+     #   "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+     #   "CacheControl": "max-age=94608000",
+    #}
 
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = "leticiasart"
