987b26e387e0f1cf4d704531a450efa1199ef879
jojo157 <carrigj@tcd.ie>
2021-10-25 19:23:15 +0000

    added test case for artist app

--- artist/tests.py
+++ artist/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+class AllArtistViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='general', password='12test12', email='general@example.com')
+        self.user.save()
+        self.timestamp = date.today()
+        self.client.login(username='general', password='12test12')
+    
+    def tearDown(self):
+        self.user.delete()
+
+    def test_only_artist_can_access_restricted_views(self):
+        response = self.client.get('/artist')
+        self.assertRedirects(response, '/')
+
+    
